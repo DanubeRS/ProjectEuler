483749318d0b65a1f3983ac92a2215c292670789
Daniel Park <DanubeRS@gmail.com>
2012-11-12 20:20:09 +1000

    Commented and documented solution. Reduced intermediate code in isMultiple method.

--- src/pr001/Problem.java
+++ src/pr001/Problem.java
@@ -1,27 +1,41 @@
 package pr001;
 
 /**
- * Created with IntelliJ IDEA. User: Daniel Date: 11/3/12 Time: 7:55 PM * ==================== * This is an
- * implementation of problem 1 in the ProjectEuler.net set. See http://projecteuler.net/problem=1 *
- * ====================
+ * @author Daniel J. Park DanubeRS@gmail.com
+ * @version 1.0
+ * @since 1.0
  */
 public class Problem {
 
+    //Define assisting, and operational variables
     private int upperLimit, sum = 0;
-    private int[] multiples;
+    private int[] multiples;        //To be populated with final listing of multiples
 
-    //Construct the Problem1 Object
+    /**
+     * Creates a Problem object with specified parameters.
+     *
+     * @param u
+     *         Upper Limit
+     * @param m
+     *         List of common multiples.
+     *
+     * @since 1.0
+     */
     public Problem(int u, int[] m) {
         upperLimit = u;
         multiples = m;
     }
 
-    //Calculate the sum
+    /**
+     * Invoked from a Problem object. Tests if in multiples list, and if true, adds to the sum.
+     *
+     * @return Sum of included multiples
+     */
     public int Calculate() {
 
         //For each number from 1 to the upper limit, test for multiplicity, and add if true
         for (int i = 1; i < upperLimit; i++) {
-            if (isMultiple(multiples, i)) {
+            if (isMultiple(i)) { //Tests for multiplicity
                 sum += i;
             }
         }
@@ -29,20 +43,35 @@ public class Problem {
 
     }
 
-    private boolean isMultiple(int[] mul, int subject) {
-        for (int i = 0; i < mul.length; i++) {
-            if (subject % mul[i] == 0) {return true;}
+    /**
+     * Private method tests if a passed int contains a multiple.
+     *
+     * @param subject
+     *         Integer to be tested
+     *
+     * @return True of multiple, false otherwise
+     */
+    private boolean isMultiple(int subject) {
+        for (int multiple : multiples) {
+            if (subject % multiple == 0) {return true;} // If modulus is zero, then the value is a whole factor
         }
         return false;
     }
 
+    /**
+     * The main running class. Invoked on program start
+     *
+     * @param args
+     *         Command line arguments (Not required)
+     */
     public static void main(String[] args) {
 
-        pr001.Problem pr1 = new pr001.Problem(1000, new int[]{3, 5});
+        pr001.Problem pr1 = new pr001.Problem(1000, new int[]{3, 5});   //Initialise new problem object
+
+        //Console Output
         System.out.println("Multiple addition:");
         System.out.println(pr1.Calculate());
 
     }
-
 
 }
