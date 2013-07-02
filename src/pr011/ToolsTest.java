36eb605e03334216043f9e66a506da207e96a977
Daniel Park <DanubeRS@gmail.com>
2013-07-02 23:29:16 +1000

    De3leted Java files

--- src/pr011/ToolsTest.java
+++
@@ -1,9 +0,0 @@
-package pr011;
-
-
-/**
- * Created with IntelliJ IDEA. User: Danie Date: 11/12/12 Time: 6:20 PM To change this template use File | Settings |
- * File Templates.
- */
-public class ToolsTest extends junit.framework.TestCase {
-}
